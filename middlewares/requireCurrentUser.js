8e8d1411a0ac666263c20956244c2e0617f30082
Nicolas Dantas <nicolas.dantas@gmail.com>
2021-02-04 12:04:22 +0100

    added npm i in docker configuration

--- middlewares/requireCurrentUser.js
+++ middlewares/requireCurrentUser.js
@@ -1,5 +1,5 @@
-const { UnauthorizedError } = require('../error-types');
-const User = require('../models/users');
+const { UnauthorizedError } = require("../error-types");
+const User = require("../models/users");
 
 module.exports = async (req, res, next) => {
   req.currentUser = await User.findOne(req.session.userId, false);
